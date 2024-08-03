b9dc38b1a04f2849b12b71f91d70ded350988d2d
Ayaz <ayazbaig@hotmail.co.uk>
2024-08-04 00:11:27 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true, // This makes it a 308 permanent redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
